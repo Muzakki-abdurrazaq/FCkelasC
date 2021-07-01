4bb920c298159d8aebe8dd3bd3063621f9044f7a
Muzakki Abdurrazaq <muzakkiabdurrazaq123@gmail.com>
2021-07-01 09:42:13 +0700

    Activity11

--- FBkelasA/app/src/main/java/com/example/fbkelasa/LihatTeman.java
+++ FBkelasA/app/src/main/java/com/example/fbkelasa/LihatTeman.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
+//
 public class LihatTeman extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private RecyclerView rcView;
